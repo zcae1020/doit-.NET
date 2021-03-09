625f4e69a8432d408ca0f66d777dbe14ade7af22
MinShiGee <MinShiGee@gmail.com>
2021-03-09 19:21:55 +0900

    week1 init

---
+++ HW02.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace doit_study_homework_template
+{
+    public class HW02 : homework
+    {
+        /* Dont Touch */
+        public bool isRunning { get;  set; }
+        public HW02(bool _isRunning){
+            isRunning = _isRunning;
+        }
+        /********************/
+        public void main(){
+            /* 여기에 코드를 작성하세요 */
+        }
+    }
+}
