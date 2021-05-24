6d82794648a73a2668f15ccd8406ac49058b3bb3
zcae1020 <zcae1020@naver.com>
2021-05-24 20:09:37 +0900

    week8 init

--- Data/Controllers/TestScoresController.cs
+++ Data/Controllers/TestScoresController.cs
@@ -18,38 +18,80 @@ namespace StudentManager.Data.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<TestScore>>> GetAsync(){
+        public async Task<ActionResult<List<TestScore>>> GetAsync() =>
             //Service의 Scores를 Return
-        }
+            await _testScoreTest.GetTestScoresAsync();
 
         [HttpGet("{id:length(24)}", Name = "GetTestScore")]
         public async Task<ActionResult<TestScore>> GetAsync(string id)
         {
             //파라미터 ID를 DB Id로 갖는 Score를 Return
+            var testScore = await _testScoreTest.GetAsync(id);
+
+            if (testScore == null)
+            {
+                return NotFound();
+            }
+
+            return testScore;
         }
 
         [HttpGet("testscoreId={id:length(4)}")]
         public async Task<ActionResult<TestScore>> GetByIdAsync(string id)
         {
             //파라미터 ID를 StudentId로 갖는 Score를 Return
+            var testScore = await _testScoreTest.GetByTestScoreIdAsync(id);
+
+            if (testScore == null)
+            {
+                return NotFound();
+            }
+
+            return testScore;
         }
 
         [HttpPost]
         public async Task<ActionResult<TestScore>> CreateAsync(TestScore_Dto scoreDto)
         {
             //파라미터 Score를 DB에 생성.
+            var testScore = new TestScore(scoreDto);
+            await _testScoreTest.CreateAsync(testScore);
+
+            return CreatedAtRoute("GetTestScore", new { id = testScore.Id.ToString() }, testScore);
         }
 
         [HttpPut("{id:length(24)}")]
         public async Task<IActionResult> UpdateAsync(string id, TestScore_Dto scoreDtoIn)
         {
             //파라미터 ID를 DB Id로 갖는 Score를 scoreDtoIn내용으로 업테이트.
+            var testScore = await _testScoreTest.GetAsync(id);
+            var scoreIn = new TestScore(scoreDtoIn);
+            scoreIn.Id = id;
+
+            if (testScore == null)
+            {
+                return NotFound();
+            }
+
+            await _testScoreTest.UpdateAsync(id, scoreIn);
+
+            return NoContent();
         }
 
         [HttpDelete("{id:length(24)}")]
         public async Task<IActionResult> DeleteAsync(string id)
         {
             //파라미터 ID를 DB Id로 갖는 Score를 제거.
+            var score = await _testScoreTest.GetAsync(id);
+
+            if (score == null)
+            {
+                return NotFound();
+            }
+
+            await _testScoreTest.Remove(score.Id);
+
+            return NoContent();
         }
     }
 }
--- Data/Services/StudentService.cs
+++ Data/Services/StudentService.cs
@@ -19,35 +19,38 @@ namespace StudentManager.Data.Services
             _students = database.GetCollection<Student>(settings.StudentsCollectionName);
         }
 
-        public async Task<List<Student>> GetStudentsAsync(){
+        public async Task<List<Student>> GetStudentsAsync() =>
             //_student를 Return
-        }
+            await _students.Find(st => true).ToListAsync();
 
-        public async Task<Student> GetAsync(string id){
+        public async Task<Student> GetAsync(string id) =>
             //DB Id가 파라미터 id와 동일한 student Return.
-        }
+            await _students.Find<Student>(student => student.Id == id).FirstOrDefaultAsync();
 
-        public async Task<Student> GetByStudentIdAsync(string id){
+        public async Task<Student> GetByStudentIdAsync(string id) =>
             //StudentId가 파라미터 id와 동일한 student Return.
-        }
+            await _students.Find(student => student.studentId == id).FirstOrDefaultAsync();
 
         public async Task CreateAsync(Student student){
             //파라미터 student를 DB에 추가.
+            await _students.InsertOneAsync(student);
         }
 
-        public async Task UpdateAsync(string id, Student studentIn){
+        public async Task UpdateAsync(string id, Student studentIn) =>
             //DB Id가 파라미터 id와 동일한 Student를 studentIn으로 업데이트.
-        }
+            await _students.ReplaceOneAsync(student => student.Id == id, studentIn);
 
         public async Task RemoveByStudentIdAsync(string id){
             //StudentId가 파라미터 id와 동일한 Student를 제거.
+            Student student = await GetByStudentIdAsync(id);
+            await _students.DeleteOneAsync(s => s.Id == student.Id);
         }
-        public async Task RemoveAsync(Student studentIn){
+        public async Task RemoveAsync(Student studentIn) =>
             //파라미터의 studentIn와 동일한 내용의 Student 제거.
-        }
+            await _students.DeleteOneAsync(s => s.Id == studentIn.Id);
 
-        public async Task Remove(string id){
+        public async Task Remove(string id) =>
             //DB Id가 파라미터의 id와 동일한 Student 제거.
-        }
+            await _students.DeleteOneAsync(s => s.Id == id);
     }
 }
